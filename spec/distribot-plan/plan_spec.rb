9f0602c14d72ac10c7c56789d60377c757e260a6
John Drago <jdrago.999@gmail.com>
2016-03-16 21:55:37 -0700

    Moving forward with tests

--- spec/distribot-plan/plan_spec.rb
+++ spec/distribot-plan/plan_spec.rb
@@ -3,31 +3,5 @@ require 'spec_helper'
 describe Distribot::Plan do
   it 'is a class' do
     expect(Distribot::Plan).to be_a Class
-Distribot.plan :greeting do
-  task :get_name do
-    handler 'HandlerTo::GetName'
-  end
-
-  group :decide_greeting_context, depends_on: :get_name do
-    handler 'HandlerTo::GetTimeOfDay'
-    handler 'HandlerTo::GuessGender'
-    handler 'HandlerTo::DetermineSocialStatus'
-  end
-
-  task :assemble_greeting, depends_on: :decide_greeting_context do
-    handler 'HandlerTo::AssembleGreeting'
-  end
-
-  task :wait_for_proper_timing, depends_on: :assemble_greeting do
-    handler 'HandlerTo::WaitForProperTiming'
-  end
-
-  task :say_greeting, depends_on: :wait_for_proper_timing do
-    handler 'HandlerTo::SayGreeting'
-  end
-end
-byebug
-puts 'now....'
-
   end
 end
---
+++ spec/distribot-plan/task_group_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Distribot::TaskGroup do
+  it 'is a class' do
+    expect(Distribot::TaskGroup).to be_a Class
+  end
+end
